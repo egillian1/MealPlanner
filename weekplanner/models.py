0fa890f64c94e8d64bba97b8192b0e3296838f0e
egill.ian1@gmail.com <egill.ian1@gmail.com>
2018-02-25 16:29:02 +0000

    Admin site added

--- weekplanner/models.py
+++ weekplanner/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    recipe_name = models.CharField(max_length=300)
+    recipe_url = models.CharField(max_length=400)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.recipe_name
