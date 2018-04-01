102fe09830c818ab8c98fd53de8aa1bb3e53b774
egill.ian1@gmail.com <egill.ian1@gmail.com>
2018-04-01 19:56:51 +0100

    further authentication modifications

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'authentication'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', views.login, name='login'),
+    path('login', views.logIntoSite, name='logIntoSite'),
     path('register', views.register, name='register'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 #from django.core.exceptions import ValidationError
 
 from pprint import pprint
@@ -10,8 +12,15 @@ def index(request):
     #return HttpResponse("You are at the login page")
     return render(request, 'authentication/login.html')
 
-def login(request):
-    return HttpResponse("Congration. You done it (log in).")
+def logIntoSite(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        #return reverse('mealplanner:index')
+    else:
+        return HttpResponse("Login failed.")
 
 def register(request, error = ""):
     if(request.method == "GET"):
@@ -24,7 +33,12 @@ def register(request, error = ""):
 
     if(password != confirm):
         logger.info('passwords do not match')
-        #raise ValidationError("Passwords do not match")
         return render(request, 'authentication/register.html', {'error': 'Passwords do not match'})
 
+    if(len(password) < 7):
+        logger.info('password too short')
+        return render(request, 'authentication/register.html', {'error': 'Password is too short'})
+
+    newUser = User.objects.create_user(username, username, password)
+    newUser.save()
     return HttpResponse("Registered successfully")
